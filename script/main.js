0ad38248fe5a6390ce749562dffde0e171a5b7e8
Davide Saporita <davide.saporita@gmail.com>
2020-04-15 15:07:36 +0200

    Added project files

---
+++ script/main.js
@@ -0,0 +1,12 @@
+/**
+ *  SIMON SAYS
+ * 
+ *  Un alert espone 5 numeri casuali (univoci). 
+ *  Da li parte un timer di 30 secondi.
+ *  Dopo 30 secondi l’ utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
+ *  Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati
+ *  Assicuratevi di saper fare correttamente l’ esercizio con output base (alert() , prompt() , console.log() )
+ * 
+ * 
+ */
+
