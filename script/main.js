12d6737b46cb7bd6aac63add74ef160f006b6811
Davide Saporita <davide.saporita@gmail.com>
2020-04-15 16:10:08 +0200

    JS basic functionality completed

--- script/main.js
+++ script/main.js
@@ -9,4 +9,53 @@
  * 
  * 
  */
+var level = 5;
+var rangeMin = 1;
+var rangeMax = 100;
 
+var numbers = getRandomInt(level, rangeMin, rangeMax);
+alert('Ricorda questi numeri:\n' + numbers);
+
+
+
+setTimeout(function() {
+    alert('Te li ricordi?');
+    var temp = 0;
+    var arrayUser = [];
+    for ( i = 0; i < level ; i++ ){
+        while ( temp == 0 || isNaN(temp) ) {
+            temp = parseInt(prompt('Inserisci il ' + (i+1) + '° numero che ricordi '));
+        } 
+        arrayUser.push(temp);
+        temp = 0;
+    }
+    console.log('I dati da te inseriti sono: ' + arrayUser);
+    console.log('I numeri da ricordare erano: ' + numbers);
+    
+},2000);
+
+
+function getRandomInt(x = 5, min = 1, max = 100) { // x --> quanti numeri generare (max 100) | min & max --> range 
+    var array = [];
+    var temp = 0;
+    var check = false;
+    var range = max - min;
+
+    if (x > 0 && x <= 100 && min > 0 && range >= x-1) {
+        check = true;
+    } else {
+        check = false;
+    }
+
+    if ( check ) {        
+        while ( array.length < x ) {
+            temp = Math.floor(Math.random() * (range +1 )) + min;
+            if ( !array.includes(temp) ) {
+                array.push(temp);
+            }
+        }
+        return array;
+    } else {
+        return false;
+    }
+}
