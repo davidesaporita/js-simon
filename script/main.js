d1ac52be4ca3250861d482eeecb10627689e633a
Davide Saporita <davide.saporita@gmail.com>
2020-04-16 10:05:15 +0200

    --Simon style UI-- developed

--- script/main.js
+++ script/main.js
@@ -9,53 +9,99 @@
  * 
  * 
  */
-var level = 5;
-var rangeMin = 1;
-var rangeMax = 100;
 
-var numbers = getRandomInt(level, rangeMin, rangeMax);
-alert('Ricorda questi numeri:\n' + numbers);
+// References
 
 
+var level = 1;
+var green = document.querySelector('.spicchio.green');
+var red = document.querySelector('.spicchio.red');
+var blue = document.querySelector('.spicchio.blue');
+var yellow = document.querySelector('.spicchio.yellow');
+var sections = document.querySelectorAll('.spicchio');
 
-setTimeout(function() {
-    alert('Te li ricordi?');
-    var temp = 0;
-    var arrayUser = [];
-    for ( i = 0; i < level ; i++ ){
-        while ( temp == 0 || isNaN(temp) ) {
-            temp = parseInt(prompt('Inserisci il ' + (i+1) + '° numero che ricordi '));
-        } 
-        arrayUser.push(temp);
-        temp = 0;
-    }
-    console.log('I dati da te inseriti sono: ' + arrayUser);
-    console.log('I numeri da ricordare erano: ' + numbers);
+play(level);
+label('LEVEL ' + level);
+console.log('----------\nLivello: ' + level);
+
+function play(level) {
+
     
-},2000);
+    var combination = randomCombo(level);
+    var y = 0;
+    var timing = setInterval(function() {
+        if( y < combination.length ) {
+            switch(combination[y]) {
+                case 0: flash(green, level); break;
+                case 1: flash(red, level); break;
+                case 2: flash(yellow, level); break;
+                case 3: flash(blue, level); break;
+            }    
+            y++;
+        } else {
+            clearInterval(timing);
+        }
+    },500);
+    newLevel = listen(combination, level);
+}
 
 
-function getRandomInt(x = 5, min = 1, max = 100) { // x --> quanti numeri generare (max 100) | min & max --> range 
-    var array = [];
-    var temp = 0;
-    var check = false;
-    var range = max - min;
+function listen(combination, level) {
+    var array = [];   
+    green.addEventListener('click',  () => { array.push(0); flash(green, level, array, combination, true);  });
+    red.addEventListener('click',    () => { array.push(1); flash(red, level, array, combination, true);    });
+    yellow.addEventListener('click', () => { array.push(2); flash(yellow, level, array, combination, true); });
+    blue.addEventListener('click',   () => { array.push(3); flash(blue, level, array, combination, true);   });
+}
 
-    if (x > 0 && x <= 100 && min > 0 && range >= x-1) {
-        check = true;
-    } else {
-        check = false;
-    }
+function flash(ref, level, array = [], combination = [], listening = false){
+    var win = true;
+    ref.classList.add('active');
+    setTimeout(() => { ref.classList.remove('active'); }, 400);
+    
+    if(listening){
+        if (array.length == level) {
+            for( var i = 0; i < combination.length; i++ ) {
+                if( combination[i] === array[i] ) {
+                    console.log((i+1) + ' | Bravo!');
+                } else {
+                    console.log((i+1) + ' | Sbagliato!');
+                    win = false;
+                }
+            }
 
-    if ( check ) {        
-        while ( array.length < x ) {
-            temp = Math.floor(Math.random() * (range +1 )) + min;
-            if ( !array.includes(temp) ) {
-                array.push(temp);
+            if(win){
+                level++;
+                console.log('----------\nLivello: ' + level);
+                setTimeout(function(){
+                    label('LEVEL ', level);
+                    setTimeout(function(){
+                        play(level);
+                    }, 1000);
+                }, 1000);
+                
+            } else {
+                label('YOU LOSE');
+                console.log('hai perso hahahahahha!');
             }
         }
-        return array;
-    } else {
-        return false;
     }
+}
+
+function randomCombo( x = 1 ) {
+    var colors = ['r','g','b','y'];
+    var combo = [];
+    for( var i = 0; i < x; i++ ){
+        combo.push(Math.floor(Math.random() * colors.length));
+    }
+    return combo;
+}
+
+function label(string, level = false) {
+    if(level) {
+        document.getElementById('label').innerHTML = string + level;
+    } else {
+        document.getElementById('label').innerHTML = string;
+    }
+    
 }
